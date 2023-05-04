26b7b4e208372a28795c3aa43a8dd57a421a6f14
RyanSilv257 <130668947+RyanSilv257@users.noreply.github.com>
2023-05-04 12:13:04 -0300

    commit do duente

---
+++ Conversor.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Conversor {
+    public static void main(String[] args) {
+        
+        //Declaração de variáveis
+        int cel, fah, op;
+
+        //Criando instância do objeto de entrada
+        Scanner entrada = new Scanner(System.in);
+
+        do{
+            System.out.println("\n\t\t\t -- Conversor de Temperatura \n");
+
+            System.out.println("1. Celsius para Fahrenheit");
+            System.out.println("2. Fahrenheit para Celsius");
+            System.out.println("3. Sair");
+
+            System.out.print("Opção: ");
+                op = entrada.nextInt();
+
+        if(op == 1){
+            System.out.println("\n\t\t\t -- Celsius para Fahrenheit -- \n");
+
+            //Entrada
+            System.out.print("Informe os graus Celsius: ");
+                    cel = entrada.nextInt();
+
+            //Processamento
+            fah = cel * 9 / 5 + 32;
+
+            //Saída
+            System.out.println(fah + "º Fahrenheit");
+
+        } else if(op == 2){
+            System.out.println("\n\t\t\t -- Fahrenheit para Celsius -- \n");
+
+
+        } else if(op == 3){
+            System.out.println("Forte Abraço!");
+        
+        }else{
+            System.out.println("Opção " + op + " incorreta!");
+        }
+
+    }while(op!=3);
+    }
+
+}
